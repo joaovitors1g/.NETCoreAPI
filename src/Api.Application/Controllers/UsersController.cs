4fae6acd29d3e8168e6f1c39b9761213833b1b3e
joaovitors1g <joao_vitor9846@hotmail.com>
2020-11-15 13:28:28 -0300

    feat: add create and update user endpoints

--- src/Api.Application/Controllers/UsersController.cs
+++ src/Api.Application/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using Api.Domain.Entities;
 using Api.Domain.Interfaces.Services.User;
 using Microsoft.AspNetCore.Mvc;
 
@@ -49,6 +50,62 @@ namespace Api.Application.Controllers
       }
       catch (ArgumentException ex)
       {
+        return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+      }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create([FromBody] UserEntity user)
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      try
+      {
+        var result = await _service.Post(user);
+
+        if (result != null)
+        {
+          return Created(new Uri(Url.Link("GetWithId", new { result.Id })), result);
+        }
+        else
+        {
+          return BadRequest();
+        }
+      }
+      catch (Exception ex)
+      {
+
+        return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+      }
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> Update([FromBody] UserEntity user)
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      try
+      {
+        var result = await _service.Put(user);
+
+        if (result != null)
+        {
+          return Ok(result);
+        }
+        else
+        {
+          return BadRequest();
+        }
+      }
+      catch (Exception ex)
+      {
+
         return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
       }
     }
