393cb34818898eb53cb8157675e59a5d4d53b2a9
joaovitors1g <joao_vitor9846@hotmail.com>
2020-12-21 10:14:41 -0300

    feat: add default user dto

---
+++ src/Api.Domain/DTOs/User/UserDTO.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Api.Domain.DTOs.User
+{
+    public class UserDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
