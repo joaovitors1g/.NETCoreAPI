c14aba37cd6312f762782b3220f3720645b6e575
joaovitors1g <joao_vitor9846@hotmail.com>
2020-12-21 08:37:43 -0300

    feat: add jwt auth in controllers

--- src/Api.Application/Controllers/UsersController.cs
+++ src/Api.Application/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using Api.Domain.DTOs.User;
 using Api.Domain.Entities;
 using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Application.Controllers
@@ -17,6 +19,7 @@ namespace Api.Application.Controllers
          _service = service;
       }
 
+      [Authorize("Bearer")]
       [HttpGet]
       public async Task<ActionResult> Index()
       {
@@ -35,6 +38,7 @@ namespace Api.Application.Controllers
          }
       }
 
+      [Authorize("Bearer")]
       [HttpGet]
       [Route("{id}", Name = "GetWithId")]
       public async Task<ActionResult> View(Guid id)
@@ -54,8 +58,9 @@ namespace Api.Application.Controllers
          }
       }
 
+      [Authorize("Bearer")]
       [HttpPost]
-      public async Task<ActionResult> Create([FromBody] UserEntity user)
+      public async Task<ActionResult> Create([FromBody] UserDTO user)
       {
          if (!ModelState.IsValid)
          {
@@ -82,8 +87,9 @@ namespace Api.Application.Controllers
          }
       }
 
+      [Authorize("Bearer")]
       [HttpPut]
-      public async Task<ActionResult> Update([FromBody] UserEntity user)
+      public async Task<ActionResult> Update([FromBody] UserDTO user)
       {
          if (!ModelState.IsValid)
          {
@@ -108,7 +114,8 @@ namespace Api.Application.Controllers
             return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
          }
       }
-
+      
+      [Authorize("Bearer")]
       [HttpDelete("{id}")]
       public async Task<ActionResult> Delete(Guid id)
       {
